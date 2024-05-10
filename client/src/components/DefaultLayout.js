844315c62e5b817c167705c9dd404014fb8f984b
iamajay01 <ajaykarthik.eng@gmail.com>
2024-05-10 16:28:41 +0530

    updated logo

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -67,7 +67,7 @@ function DefaultLayout({ children }) {
     <div className="layout-parent">
       <div className="sidebar">
         <div className="sidebar-header">
-             <h1 className="logo">SB</h1>
+             <h1 className="logo">MB</h1>
              <h1 className="role">{user?.name} <br />Role : {user?.isAdmin ? 'Admin' : 'User'}</h1>
         </div>
         <div className="d-flex flex-column gap-3 justify-content-start menu">
